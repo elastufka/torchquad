54ffa387752084341ccadbd80bdbfa3a6b201ddc
Erica <elastufka@gmail.com>
2022-04-14 10:21:52 +0200

    try flatten

--- torchquad/integration/gauss_legendre.py
+++ torchquad/integration/gauss_legendre.py
@@ -1,71 +1,11 @@
 import torch
-import numpy
+#import numpy
 from loguru import logger
 from autoray import numpy as anp
 from autoray import do
-from scipy.special import lpmv
 
 from .base_integrator import BaseIntegrator
 from .utils import _setup_integration_domain
-
-
-def gauss_legendre0(x1, x2, npoints):
-    """
-    Calculate the positions and weights for a Gauss-Legendre integration scheme.
-
-    Parameters
-    ----------
-    x1 : `numpy.array`
-
-    x2 : `numpy.array`
-
-    npoints : `int`
-        Degree or number of points to create
-    Returns
-    -------
-    `tuple` :
-        (x, w) The positions and weights for the integration.
-
-    Notes
-    -----
-
-    Adapted from SSW
-    `Brm_GauLeg54.pro <https://hesperia.gsfc.nasa.gov/ssw/packages/xray/idl/brm/brm_gauleg54.pro>`_
-    """
-    eps = 3e-14
-    m = (npoints + 1) // 2
-
-    x = numpy.zeros((x1.size, npoints))
-    w = numpy.zeros((x1.size, npoints))
-
-    # Normalise from -1 to +1 as Legendre polynomial only valid in this range
-    xm = 0.5 * (x2 + x1)
-    xl = 0.5 * (x2 - x1)
-
-    for i in range(1, m + 1):
-
-        z = numpy.cos(numpy.pi * (i - 0.25) / (npoints + 0.5))
-        # Init to np.inf so loop runs at least once
-        z1 = numpy.inf
-
-        # Some kind of integration/update loop
-        while numpy.abs(z - z1) > eps:
-            # Evaluate Legendre polynomial of degree npoints at z points P_m^l(z) m=0, l=npoints
-            p1 = lpmv(0, npoints, z)
-            p2 = lpmv(0, npoints - 1, z)
-
-            pp = npoints * (z * p1 - p2) / (z ** 2 - 1.0)
-
-            z1 = numpy.copy(z)
-            z = z1 - p1 / pp
-
-        # Update ith components
-        x[:, i - 1] = xm - xl * z
-        x[:, npoints - i] = xm + xl * z
-        w[:, i - 1] = 2.0 * xl / ((1.0 - z ** 2) * pp ** 2)
-        w[:, npoints - i] = w[:, i - 1]
-
-    return x, w
 
 class GaussLegendre(BaseIntegrator):
     """Gauss Legendre quadrature rule in torch. See https://en.wikipedia.org/wiki/Gaussian_quadrature#Gauss%E2%80%93Legendre_quadrature."""
@@ -74,8 +14,27 @@ class GaussLegendre(BaseIntegrator):
         super().__init__()
         
     def _gauss_legendre(self,n):
-        """returns Gauss-Legendre points and weights for degree n and dimension self._dim"""
-        return do("polynomial.legendre.leggauss",n,like="numpy")#anp.polynomial.legendre.leggauss(n)
+        """returns Gauss-Legendre points and weights for degree n and dimension self._dim
+        Adjusts from interval [-1,1] to integration limits [a,b]"""
+        a,b=self._integration_domain.T
+        xi, wi = do("polynomial.legendre.leggauss",n,like="numpy")
+        #scale from [-1,1] to [a,b] e.g. https://en.wikipedia.org/wiki/Gaussian_quadrature#Change_of_interval
+        
+        xm=0.5*(b+a)
+        xl=0.5*(b-a)
+        if isinstance(xm,torch.Tensor):
+            #for now... figure out a better solution later
+            aa=torch.zeros(n)
+            xi=aa.new(xi)
+            wi=aa.new(wi)
+
+        if xm.device !='cpu':
+            xi=do("repeat",xm,n,like="numpy").reshape(self._dim,npoints)+anp.outer(xl,xi)
+            #now xm has to be on CPU ... but xi needs to be on GPU
+        else:
+            xi=do("repeat",xm.cpu(),n,like="numpy").reshape(self._dim,npoints).to(torch.cuda.current_device())+anp.outer(xl,xi)
+        wi=anp.outer(wi,xl).T
+        return xi,wi
 
     def integrate(self, fn, dim, args=None, N=2, eps_abs=None, eps_rel=1e-3, max_N=12, base=2, integration_domain=None, fixed=False):
         """Integrates the passed function on the passed domain using Gauss-Legendre quadrature.
@@ -107,61 +66,49 @@ class GaussLegendre(BaseIntegrator):
         self._dim = dim
         self._fn = fn
         #with torch.no_grad(): #should already be like this since no nn
-        @torch.jit.script
-        def test_jit(N:int=N,max_N : int=max_N,base :int =base, eps_abs=None,eps_rel:float=eps_rel,args=None):
-            for ires in range(N, max_N + 1): #if starting at npoints=8
-                npoints = base ** ires #is this standard?
-                #print(f"npoints {npoints}")
-                if npoints > base**max_N:
-                    raise ValueError(f"Integral did not satisfy the conditions eps_abs={eps_abs} or eps_rel={eps_rel} using the maximum number of points {base**max_N}") #or a different error?
-                    break
 
-                # generate positions and weights
-                a,b=self._integration_domain.T
-                xi, wi = gauss_legendre0(a,b,npoints) #numpy.polynomial.legendre.leggauss(npoints)#self._gauss_legendre(npoints)  #(dim,n)
-                #scale from [-1,1] to [a,b] e.g. https://en.wikipedia.org/wiki/Gaussian_quadrature#Change_of_interval
-                #
-                #xm=0.5*(b+a)
-                #xl=0.5*(b-a)
-                #if isinstance(xm,torch.Tensor):
-                #    #for now... figure out a better solution later
-                #    aa=torch.randn(npoints)
-                #    xi=aa.new(xi)
-                #    wi=aa.new(wi)
+        all_points,all_weights=[],[]
+        for ires in range(N, max_N + 1): #if starting at npoints=8
+            npoints = base ** ires #is this standard?
+            
+#            if npoints > base**max_N:
+#                raise ValueError(f"Integral did not satisfy the conditions eps_abs={eps_abs} or eps_rel={eps_rel} using the maximum number of points {base**max_N}") #or a different error?
+#                break
 
-                #if xm.device !='cpu':
-                    #xi=do("repeat",xm,npoints,like="numpy").reshape(self._dim,npoints)+anp.outer(xl,xi)
-                    #now xm has to be on CPU ... but xi needs to be on GPU
-                #else:
-                    #xi=do("repeat",xm.cpu(),npoints,like="numpy").reshape(self._dim,npoints).to(torch.cuda.current_device())+anp.outer(xl,xi)
-                #wi=anp.outer(wi,xl).T
-                
-                logger.debug("Evaluating integrand for {xi}.")
-                if self._nr_of_fevals > 0:
-                    lastsum = anp.array(integral)
-                    integral[i] = anp.sum(self._eval(xi[i], args=args)*wi[i],axis=1) #[a[i.tolist()] for a in args]
-                else:
-                    integral = torch.sum(self._eval(xi,args=args)*wi,axis=1) #integral from a to b f(x) ≈ sum (w_i*f(x_i))
-                    if fixed:
-                        break #no error evaluation if fixed-point quadrature desired
+            # generate positions and weights
+            xi, wi = self._gauss_legendre(npoints)
+            all_points.append(xi.flatten())
+            all_weights.append(wi.flatten())
+            
+        #logger.debug("Evaluating integrand for {xi}.")
+        all_eval=self._eval(xi[i], args=args)*wi[i]
+        
+        #now reshape based on dim, npoints... does this have to be done in loop? booo.....
+    
+        if self._nr_of_fevals > 0:
+            lastsum = anp.array(integral)
+            integral[i] = anp.sum(self._eval(xi[i], args=args)*wi[i],axis=1) #[a[i.tolist()] for a in args]
+        else:
+            integral = torch.sum(self._eval(xi,args=args)*wi,axis=1) #integral from a to b f(x) ≈ sum (w_i*f(x_i))
+            if fixed:
+                break #no error evaluation if fixed-point quadrature desired
 
-                #print(npoints,integral)
-                # Convergence criterion
-                if self._nr_of_fevals//self._dim > 1:
-                    l1 = anp.abs(integral - lastsum)
-                    if eps_abs is not None:
-                        i = anp.where(l1 > eps_abs)[0]
-                    if eps_rel is not None:
-                        l2 = eps_rel * anp.abs(integral)
-                        i = anp.where(l1 > l2)[0]
-                else:
-                    i= anp.arange(self._dim) #indices of integral
+        #print(npoints,integral)
+        # Convergence criterion
+        if self._nr_of_fevals//self._dim > 1:
+            l1 = anp.abs(integral - lastsum)
+            if eps_abs is not None:
+                i = anp.where(l1 > eps_abs)[0]
+            if eps_rel is not None:
+                l2 = eps_rel * anp.abs(integral)
+                i = anp.where(l1 > l2)[0]
+        else:
+            i= anp.arange(self._dim) #indices of integral
 
-                # If all point have reached criterion return value
-                if i.size == 0:
-                    break
+        # If all point have reached criterion return value
+        if i.size == 0:
+            break
 
-            logger.info(f"Computed integral was {integral}.")
-            return integral
-        _ = test_jit() #compile?
-        return test_jit()
+        #logger.info(f"Computed integral was {integral}.")
+        return integral
+
