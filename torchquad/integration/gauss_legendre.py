ad7cef1628023346dabc97340079c02ae0567f35
Erica <elastufka@gmail.com>
2022-04-20 16:07:47 +0200

    fixed-point GL quadrature

--- torchquad/integration/gauss_legendre.py
+++ torchquad/integration/gauss_legendre.py
@@ -14,8 +14,15 @@ class GaussLegendre(BaseIntegrator):
         super().__init__()
         
     def _gauss_legendre(self,n):
-        """returns Gauss-Legendre points and weights for degree n and dimension self._dim
-        Adjusts from interval [-1,1] to integration limits [a,b]"""
+        """Returns Gauss-Legendre points and weights for degree n and dimension self._dim
+        Adjusts from interval [-1,1] to integration limits [a,b]
+        
+        Args:
+            n (int): Degree to use for computing sample points and weights, which will correctly integrate polynomials of degree 2*deg-1 or less. Generation of Gauss-Legendre points have only been tested up to degree 100.
+        
+        Returns:
+            tuple(points, weights)
+            """
         a,b=self._integration_domain.T
         xi, wi = do("polynomial.legendre.leggauss",n,like="numpy")
         #scale from [-1,1] to [a,b] e.g. https://en.wikipedia.org/wiki/Gaussian_quadrature#Change_of_interval
@@ -30,90 +37,38 @@ class GaussLegendre(BaseIntegrator):
 
         if xm.device !='cpu':
             xi=do("repeat",xm,n,like="numpy").reshape(self._dim,n)+anp.outer(xl,xi)
-            #now xm has to be on CPU ... but xi needs to be on GPU
         else:
-            xi=do("repeat",xm.cpu(),n,like="numpy").reshape(self._dim,n).to(torch.cuda.current_device())+anp.outer(xl,xi)
+            xi=do("repeat",xm.cpu(),n,like="numpy").reshape(self._dim,n).to(torch.cuda.current_device())+anp.outer(xl,xi) #what if backend isn't torch?
         wi=anp.outer(wi,xl).T
         return xi,wi
 
-    def integrate(self, fn, dim, args=None, N=2, eps_abs=None, eps_rel=1e-3, max_N=12, base=2, integration_domain=None, fixed=False):
-        """Integrates the passed function on the passed domain using Gauss-Legendre quadrature.
+    def integrate(self, fn, dim, args=None, N=8, integration_domain=None):
+        """Integrates the passed function on the passed domain using fixed-point Gauss-Legendre quadrature.
 
         Args:
             fn (func): The function to integrate over.
             dim (int): Dimensionality of the function to integrate.
             args (iterable object, optional): Additional arguments ``t0, ..., tn``, required by `fn`.
-            N (int, optional): Total number of sample points to use for the integration. Defaults to 2.
-            eps_abs (float, optional): Absolute error condition used to evaluate quadrature. Defaults to None
-            eps_rel (float, optional): Relative error condition used to evaluate quadrature. Defaults to 1e-3
-            max_N (int, optional): Maximum number of sample points to use for the integration. Defaults to 12.
-            base (int, optional): Base number to use for determining npoints. Defaults to 2. This means if N=2, the number of Gauss-Legendre points that the integral starts with will be base**N or 2**2=4. Likewise, the maximum number of points that the integral will evaluate will be base**max_N, or with the defaults 2**12=4096.
+            N (int, optional): Degree to use for computing sample points and weights, which will correctly integrate polynomials of degree 2*deg-1 or less. Note that this is not the total number of sample points used by the integration, which will be dim**N. Generation of Gauss-Legendre points have only been tested up to degree 100. Defaults to 8.
             integration_domain (list, optional): Integration domain, e.g. [[-1,1],[0,1]]. Defaults to [-1,1]^dim.
 
         Returns:
             float: integral value
-            
-        Basic example (works):
-            def cosfn(x): return np.cos(x)
-            gl=GaussLegendre()
-            integral=gl.integrate(cosfn,dim=10,eps_rel=1e-10)
+    
         """
         self._integration_domain = _setup_integration_domain(dim, integration_domain)
-        self._check_inputs(dim=dim, N=N, integration_domain=self._integration_domain) #might need to check more
+        self._check_inputs(dim=dim, N=N, integration_domain=self._integration_domain)
 
-        #logger.debug(f"Using Trapezoid for integrating a fn with {npoints} points over {self._integration_domain}")
+        logger.debug(f"Using GaussLegendre for integrating a fn with {N} points over {self._integration_domain}")
 
         self._dim = dim
         self._fn = fn
-        #with torch.no_grad(): #should already be like this since no nn
-
-        all_points,all_weights=[],[]
-        for ires in range(N, max_N + 1): #if starting at npoints=8
-            npoints = base ** ires #is this standard?
-            
-#            if npoints > base**max_N:
-#                raise ValueError(f"Integral did not satisfy the conditions eps_abs={eps_abs} or eps_rel={eps_rel} using the maximum number of points {base**max_N}") #or a different error?
-#                break
-
-            # generate positions and weights
-            xi, wi = self._gauss_legendre(npoints)
-            all_points.append(xi.flatten())
-            all_weights.append(wi.flatten())
-            
-        #logger.debug("Evaluating integrand for {xi}.")
-        all_eval=self._eval(torch.cat(all_points), args=args)*torch.cat(all_weights)
         
-        #now reshape based on dim, npoints... does this have to be done in loop? booo.....
-    
-        #if self._nr_of_fevals > 0:
-        lastsum = anp.sum(all_eval[:self._dim*base**N].reshape(self._dim,base**N),axis=1) #anp.array(integral)
-        if fixed:
-            return lastsum
-            
-        integral=anp.zeros_like(lastsum)
-        i= anp.arange(self._dim)
-        #try foor loop to start
-        for ires in range(N+1, max_N + 1):
-            npoints=base**ires
-            pidx=self._dim*base**(ires-1)
-            integral[i] = anp.sum(all_eval[pidx:pidx+self._dim*npoints].reshape(self._dim,npoints),axis=1) #[a[i.tolist()] for a in args]
-        #else:
-        #    integral = anp.sum(self._eval(xi,args=args)*wi,axis=1) #integral from a to b f(x) ≈ sum (w_i*f(x_i))
+        xi, wi = self._gauss_legendre(N)
+        integral= anp.sum(self._eval(xi,args=args,weights=wi)) #what if there is a sum in the function? then wi*self._eval() will have dimension mismatch
+        #if dim >1:
+        #    integral=anp.sum(integral)
+        logger.info(f"Computed integral was {integral}.")
 
-            # Convergence criterion
-            l1 = anp.abs(integral - lastsum)
-            if eps_abs is not None:
-                i = anp.where(l1 > eps_abs)[0]
-            if eps_rel is not None:
-                l2 = eps_rel * anp.abs(integral)
-                i = anp.where(l1 > l2)[0]
-            #else:
-            #    i= anp.arange(self._dim) #indices of integral
-
-            # If all point have reached criterion return value
-            if i.size == 0:
-                pass#break
-
-        #logger.info(f"Computed integral was {integral}.")
         return integral
 
